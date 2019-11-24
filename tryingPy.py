959330987c21b6722d321ee68e899ab0cee798b0
ostanleigh <purplesweatshirt@hotmail.com>
2019-11-23 20:49:17 -0500

    This is a first test configuring git from my laptop.

--- tryingPy.py
+++ tryingPy.py
@@ -10,3 +10,14 @@ def Test(a):
         print(count)
 
 Test(32)
+
+def gcdIter1(a, b):
+    for x in range(a, -1, -1):
+        if a % x == 0 and b % x == 0:
+            print ("The anwer is: ")
+            return x
+
+
+print(gcdIter1(24, 48))
+print("Now it is time for, home, home on the Range!")
+print(list(range(12, 20, 2)))
